dae1a9b680fbfbfe6e1d130833488c5ffa5b4098
GPED <Brayan>
2022-09-23 10:11:30 -0500

    make it better

--- caracterizacion_S/settings.py
+++ caracterizacion_S/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$salw61#noxchw+-329jy(ohbp+7p$_^vn5#60)ryy97va_)z+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["gpedcesmag.herokuapp.com"]
 
 
 # Application definition
